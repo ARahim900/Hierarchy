123e3f60e0790e9a53b9b5e116547628fe879ebd
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-09-19 12:18:01 +0400

    Components/icon

---
+++ WaterDropIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const WaterDropIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 21.75c4.833-2.33 8.25-6.843 8.25-11.855C20.25 5.37 16.63 2.25 12 2.25S3.75 5.37 3.75 9.895c0 5.012 3.417 9.525 8.25 11.855Z" clipRule="evenodd" />
+    </svg>
+);
